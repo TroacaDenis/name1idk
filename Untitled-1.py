6eb4fa35b8e4f172bdfee5502138c7844c7465ce
TroacaDenis <79210519+TroacaDenis@users.noreply.github.com>
2021-02-17 21:10:47 +0200

    stuff

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,15 +1,3 @@
-import random
-l=[]
-for i in range(0,10**3+1):
-    l.append(random.randrange(0,2))
-i=0
-maxim=0
-while i<len(l):
-    ok=0
-    while i<len(l) and l[i]==0:
-        ok+=1
-        i+=1
-    if ok>maxim:
-        maxim=ok
-    i+=1
-print(maxim)
+a=int(input("a="))
+b=int(input("b="))
+print(a)
